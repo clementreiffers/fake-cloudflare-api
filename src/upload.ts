a62e84be246cb053203747967a7c62b83b2b70e6
clementreiffers <reiffersclement@gmail.com>
2023-06-07 15:01:54 +0200

    refacto

--- src/upload.ts
+++ src/upload.ts
@@ -1,20 +1,14 @@
+import {allowedExtensions, s3Endpoint as endpoint, s3Region as region, s3Bucket} from './constants';
 import multer, {type FileFilterCallback, type StorageEngine} from 'multer';
 import path from 'path';
 import {type Request, type Express} from 'express';
-import {allowedExtensions} from './constants';
 import {S3Client} from '@aws-sdk/client-s3';
 import multerS3 from 'multer-s3';
 
-let dirName: string;
+const s3: S3Client = new S3Client({endpoint, region});
 
-const changeDirName = (newDirName: string) => {
-	dirName = newDirName;
-};
-
-const s3: S3Client = new S3Client({
-	endpoint: 'https://s3.fr-par.scw.cloud',
-	region: 'fr-par',
-});
+const generateS3Filename = (req: Request, file: Express.Multer.File): string =>
+	`${req.params?.accounts}/${req.params?.scripts}/${file.originalname}`;
 
 const storage: StorageEngine = multerS3({
 	s3,
@@ -23,7 +17,7 @@ const storage: StorageEngine = multerS3({
 		cb(null, {fieldName: file.fieldname});
 	},
 	key(req: Request, file: Express.Multer.File, cb) {
-		cb(null, `${req.params.accounts}/${req.params.scripts}/${file.originalname}`);
+		cb(null, generateS3Filename(req, file));
 	},
 });
 const isFileExtensionAccepted = (filename: string): boolean =>
@@ -39,4 +33,4 @@ const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallb
 
 const upload = multer({storage, fileFilter});
 
-export {upload, changeDirName};
+export {upload};
