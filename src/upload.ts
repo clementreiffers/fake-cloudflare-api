55e9739785b0543755c68ae8172928060e18a078
clementreiffers <reiffersclement@gmail.com>
2023-06-07 14:05:50 +0200

    code send in a account/scripts dir in S3

--- src/upload.ts
+++ src/upload.ts
@@ -1,4 +1,3 @@
-
 import multer, {type FileFilterCallback, type StorageEngine} from 'multer';
 import path from 'path';
 import {type Request, type Express} from 'express';
@@ -6,19 +5,25 @@ import {allowedExtensions} from './constants';
 import {S3Client} from '@aws-sdk/client-s3';
 import multerS3 from 'multer-s3';
 
-const s3Client: S3Client = new S3Client({
+let dirName: string;
+
+const changeDirName = (newDirName: string) => {
+	dirName = newDirName;
+};
+
+const s3: S3Client = new S3Client({
 	endpoint: 'https://s3.fr-par.scw.cloud',
 	region: 'fr-par',
 });
 
 const storage: StorageEngine = multerS3({
-	s3: s3Client,
+	s3,
 	bucket: 'stage-cf-worker',
 	metadata(req: Request, file: Express.Multer.File, cb) {
 		cb(null, {fieldName: file.fieldname});
 	},
 	key(req: Request, file: Express.Multer.File, cb) {
-		cb(null, file.originalname);
+		cb(null, `${req.params.accounts}/${req.params.scripts}/${file.originalname}`);
 	},
 });
 const isFileExtensionAccepted = (filename: string): boolean =>
@@ -34,4 +39,4 @@ const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallb
 
 const upload = multer({storage, fileFilter});
 
-export {upload};
+export {upload, changeDirName};
